ac2392dea78a1bf774efefb0bd6965ae41266854
Adrian Li <li.adrianmc@gmail.com>
2017-11-17 13:37:27 -0800

    provide example for styled-components usage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 import Link from "next/link";
+
+const Title = styled.h1`
+  color: red;
+`;
 
 const Homepage = () => (
   <div>
-    <h1>Home Page</h1>
+    <Title>Home Page</Title>
     <div><Link href="/find_teacher"><a>Find a Teacher</a></Link></div>
     <div><Link href="/teacher_onboarding"><a>Teacher Onboarding</a></Link></div>
     <div><Link href="/chat"><a>Chat</a></Link></div>
